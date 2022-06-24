b34cc86cdb9fd0d39a9572ac47c8526fd074b0b0
cshsboltbots <101142185+cshsboltbots@users.noreply.github.com>
2022-06-24 14:29:25 -0700

    Changed colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,30 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   daisyui: {
-    themes: ["light", "dark"],
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#0b0966",
+        
+"secondary": "#ffffff",
+        
+"accent": "#0b0966",
+        
+"neutral": "#606060",
+        
+"base-100": "#2e2e2e",
+        
+"info": "#8AB1E0",
+        
+"success": "#18B466",
+        
+"warning": "#C9A508",
+        
+"error": "#DC3D32",
+        },
+      },
+    ],
   },
   plugins: [require('daisyui')],
 }
