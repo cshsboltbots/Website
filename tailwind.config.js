39d8006a5fb6a213599cd17921e8c517519700e0
cshsboltbots <101142185+cshsboltbots@users.noreply.github.com>
2022-07-11 16:51:27 -0500

    Fixed cards problem/bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,37 +4,36 @@ module.exports = {
     themes: [
       {
         mytheme: {
-        
-"primary": "#0b0966",
-        
-"secondary": "#ffffff",
-        
-"accent": "#0b0966",
-        
-"neutral": "#606060",
-        
-"base-100": "#2e2e2e",
-        
-"info": "#8AB1E0",
-        
-"success": "#18B466",
-        
-"warning": "#C9A508",
-        
-"error": "#DC3D32",
+
+          "primary": "#0b0966",
+
+          "secondary": "#ffffff",
+
+          "accent": "#0b0966",
+
+          "neutral": "#606060",
+
+          "base-100": "#2e2e2e",
+
+          "info": "#8AB1E0",
+
+          "success": "#18B466",
+
+          "warning": "#C9A508",
+
+          "error": "#DC3D32",
         },
+
         extend: {
-          extend: {
-            gridTemplateColumns: {
-              'auto-fit': 'repeat(auto-fit, minmax(0, 1fr))',
-              'auto-fill': 'repeat(auto-fit, minmax(0, 1fr))',
-            },
-            gridTemplateRows: {
-              'auto-fit': 'repeat(auto-fit, minmax(0, 1fr))',
-              'auto-fill': 'repeat(auto-fit, minmax(0, 1fr))',
-            }
+          gridTemplateColumns: {
+            'auto-fit': 'repeat(auto-fit, minmax(0, 1fr))',
+            'auto-fill': 'repeat(auto-fit, minmax(0, 1fr))',
+          },
+          gridTemplateRows: {
+            'auto-fit': 'repeat(auto-fit, minmax(0, 1fr))',
+            'auto-fill': 'repeat(auto-fit, minmax(0, 1fr))',
           }
-        },
+        }
       },
     ],
   },
