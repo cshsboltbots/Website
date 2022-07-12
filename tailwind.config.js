0f39b4769e78f06d09b04270007695518098d6b1
cshsboltbots <101142185+cshsboltbots@users.noreply.github.com>
2022-07-12 13:36:21 -0500

    added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,14 @@ module.exports = {
           "neutral": "#606060",
 
           "base-100": "#2e2e2e",
+          
+          "info": "#8AB1E0",
 
+          "success": "#18B466",
+
+          "warning": "#C9A508",
+
+          "error": "#DC3D32",
 
         },
       },
