8de9501783fc693999dc6adcc51b80adff43dc28
David Corvaglia <71835941+corvad@users.noreply.github.com>
2023-03-28 21:08:10 -0500

    Update submit.js

--- public/functions/applications/submit.js
+++ public/functions/applications/submit.js
@@ -1,13 +1,37 @@
 export async function onRequestPost(context) {
   try {
     let input = await context.request.formData();
-    let pretty = JSON.stringify([...input], null, 2);
-    return new Response(pretty, {
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-      },
-    });
+    sendEmails(input);
+    return new Response('alert("Info Received");', { status: 200 });
   } catch (err) {
     return new Response('alert("Invalid Submission");', { status: 400 });
   }
+}
+
+export async function sendEmails(input) {
+    let send_request = new Request("https://api.mailchannels.net/tx/v1/send", {
+        "method": "POST",
+        "headers": {
+            "content-type": "application/json",
+        },
+        "body": JSON.stringify({
+            "personalizations": [{ 
+                "to": [ {"email": input.email,
+                          "name": input.name}],
+                "dkim_domain": DKIM_DOMAIN,
+                "dkim_selector": DKIM_SELECTOR,
+                "dkim_private_key": DKIM_PRIVATE_KEY,
+            }],
+            "from": {
+                "email": SENDER,
+                "name": SENDER_NAME,
+            },
+
+            "subject": "Bolt Bots Application Received",
+            "content": [{
+                "type": "text/plain",
+                "value": "Dear " + input.name + ",\n\nWe have received your application and are currently looking over it. We will try to get back to you asap.\n\n Best,\nThe Bolt Bots",
+            }],
+        }),
+    });
 }
