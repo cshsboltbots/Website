856537b7494c1eaf7ead019b91db00ab75f69fdb
David Corvaglia <71835941+corvad@users.noreply.github.com>
2023-03-28 21:55:52 -0500

    Update submit.js

--- public/functions/applications/submit.js
+++ public/functions/applications/submit.js
@@ -1,41 +1,43 @@
-export async function onRequestPost(context) {
-  try {
-    let input = await context.request.formData();
-    const r = await sendEmails(input);
-    let s = 'alert("' + r + '");'
-    return new Response(s, { status: 200 });
-  } catch (err) {
-    return new Response('alert("Invalid Submission");'+err, { status: 400 });
-  }
-}
+export default{
+  async function onRequestPost(context) {
+    try {
+      let input = await context.request.formData();
+      const r = await sendEmails(input);
+      let s = 'alert("' + r + '");'
+      return new Response(s, { status: 200 });
+    } catch (err) {
+      return new Response('alert("Invalid Submission");'+err, { status: 400 });
+    }
+  },
 
-export function sendEmails(input) {
-    let send_request = new Request("https://api.mailchannels.net/tx/v1/send", {
-        "method": "POST",
-        "headers": {
-            "content-type": "application/json",
-        },
-        "body": JSON.stringify({
-            "personalizations": [{ 
-                "to": [ {"email": input.email,
-                          "name": input.fname + " " + input.lname}],
-                "dkim_domain": env.DKIM_DOMAIN,
-                "dkim_selector": env.DKIM_SELECTOR,
-                "dkim_private_key": env.DKIM_PRIVATE_KEY,
-            }],
-            "from": {
-                "email": env.SENDER,
-                "name": env.SENDER_NAME,
-            },
+  function sendEmails(input) {
+      let send_request = new Request("https://api.mailchannels.net/tx/v1/send", {
+          "method": "POST",
+          "headers": {
+              "content-type": "application/json",
+          },
+          "body": JSON.stringify({
+              "personalizations": [{ 
+                  "to": [ {"email": input.email,
+                            "name": input.fname + " " + input.lname}],
+                  "dkim_domain": env.DKIM_DOMAIN,
+                  "dkim_selector": env.DKIM_SELECTOR,
+                  "dkim_private_key": env.DKIM_PRIVATE_KEY,
+              }],
+              "from": {
+                  "email": env.SENDER,
+                  "name": env.SENDER_NAME,
+              },
 
-            "subject": "Bolt Bots Application Received",
-            "content": [{
-                "type": "text/plain",
-                "value": "Dear " + input.fname + ",\n\nWe have received your application and are currently looking over it. We will try to get back to you asap.\n\nBest,\nThe Bolt Bots",
-            }],
-        }),
-    });
-    const r = fetch(send_request);
-    const rt = r.text();
-    return rt;
-}
+              "subject": "Bolt Bots Application Received",
+              "content": [{
+                  "type": "text/plain",
+                  "value": "Dear " + input.fname + ",\n\nWe have received your application and are currently looking over it. We will try to get back to you asap.\n\nBest,\nThe Bolt Bots",
+              }],
+          }),
+      });
+      const r = fetch(send_request);
+      const rt = r.text();
+      return rt;
+  },
+};
